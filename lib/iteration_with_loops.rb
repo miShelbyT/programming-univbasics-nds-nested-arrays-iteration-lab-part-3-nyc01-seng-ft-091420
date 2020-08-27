a49530437ee575b8fa1733edc095321e264965f4
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-27 18:18:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,6 @@ def join_nested_strings(src)
   while row < src.count do
     index = 0
     while index < src[row].count do
-<<<<<<< HEAD
       if src[row][index].class == String
         string_array << src[row][index]
       end
